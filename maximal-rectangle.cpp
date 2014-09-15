a1b99c5ee371f25838eaf7dbf5ff5640983caa08
Rand3 <Rand3@users.noreply.github.com>
2014-09-15 10:44:40 -0700

    Create maximal-rectangle.cpp

---
+++ maximal-rectangle.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+	int maximalRectangle(vector<vector<char>> &matrix) {
+		// https://oj.leetcode.com/problems/maximal-rectangle/
+		//
+		// Given a 2D binary matrix filled with 0's and 1's, find the
+		// largest rectangle containing all ones and return its area. 
+		//
+		int result = 0;
+		int rows = (int)matrix.size();
+		if (rows == 0) { return result; }
+		int cols = (int)matrix[0].size();
+		if (rows == 0) { return result; }
+		for (int i = 0; i < rows; i++) {
+			// construct height[], where each h is defined as the consecutive 
+			// number of 1's from current row to top row
+			vector<int> height(cols, 0);
+			for (int j = 0; j < cols; j++) {
+				for (int k = i; k >= 0 && matrix[k][j] == '1'; k--) {
+					height[j]++;
+				}
+			}
+			int area = largestRectangleArea(height);
+			result = max(result, area);
+		}
+		return result;
+	}
+	int largestRectangleArea(vector<int> &height) {
+		// https://oj.leetcode.com/problems/largest-rectangle-in-histogram/
+		//
+		int result = 0;
+		int size = (int)height.size();
+		struct Node {
+			int height;
+			int left;
+			Node(int h, int l) : height(h), left(l) {}
+		};
+		stack<Node> s;
+		for (int i = 0; i <= size; i++) {
+			int left = i;
+			int h = (i == size ? 0 : height[i]);
+			while (!s.empty() && h < s.top().height) {
+				left = s.top().left;
+				int area = (i - left) * s.top().height;
+				result = max(result, area);
+				s.pop();
+			}
+			// the only time when we do not push: h == s.top().height
+			while (s.empty() || h > s.top().height) {
+				s.push(Node(h, left));
+			}
+		}
+		return result;
+	}
+	void test() {
+		vector<string> m = {
+			"1011",
+			"1111",
+			"1110"
+		};
+		vector<vector<char>> matrix;
+		for (string &s : m) {
+			matrix.push_back(vector<char>(s.begin(), s.end()));
+		}
+		int result = maximalRectangle(matrix);
+		cout << result << endl;
+	}
+};
